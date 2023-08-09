a8864b3122731e0da3727a4a2282201fb6d86d40
izzatonline <izzat.online@gmail.com>
2023-08-09 15:45:05 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-// import logo from "./logo.svg";
 import "./App.css";
-// import Welcome from "./components/Welcome";
 import Clock from "./components/Clock";
 
 function App() {
-    //     const userInfo = {
-    //         firstname: "Izzat Afifi",
-    //         lastname: "Jamalul Lail",
-    //     };
-    // const element = <h1>Hello, World and Welcome to React</h1>;
     return (
         <>
             <div>
                 <h1>Hello! The Time Now is:</h1>
                 <Clock />
-
-                {/* <Welcome user="Izzat" />
-            <Welcome user="Afifi" />
-            <Welcome user="Jamalul" /> */}
             </div>
         </>
     );
